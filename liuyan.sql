308853f27f3cef1adf75f41f68bda728d5566c38
学会孤独你注定强大 <71379281+xieyuxiang-1104@users.noreply.github.com>
2020-10-20 20:15:49 +0800

    Add files via upload
    数据库

---
+++ liuyan.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.40-community : Database - liuyan
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`liuyan` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `liuyan`;
+
+/*Table structure for table `content` */
+
+DROP TABLE IF EXISTS `content`;
+
+CREATE TABLE `content` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(200) DEFAULT NULL,
+  `tame` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `content` */
+
+insert  into `content`(`id`,`content`,`tame`,`name`) values (1,'你好','2020-10-02 14:27:29','花花'),(2,'你好','2020-10-02 14:29:13','花花');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
